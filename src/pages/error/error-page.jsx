1651f3da8d3f5836f65b8a07262f44b0348e94d7
p07a1330 <p07a1330@yandex.ru>
2023-12-09 15:10:43 +0300

    with created 404 page

--- src/pages/error/error-page.jsx
+++ src/pages/error/error-page.jsx
@@ -1,6 +1,14 @@
-export const ErrorPage = (props)=>{
+import style from "./style.module.scss"
+import backgroundImage from "../../static/404.svg"
+
+export const ErrorPage = (props) => {
     console.log(props)
+    if (props.code === 404) {
+        return <div className={style.wrapper}>
+            <img src={backgroundImage} alt={"404 bg"}/>
+        </div>
+    }
     return <div>
-        I am error page
+        I am error page. I am not 404 error, so my design will be implemented later
     </div>
 }
