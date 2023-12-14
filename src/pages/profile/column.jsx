88f977ec54bc20b4c20c67cc40a5f5c2736a8223
p07a1330 <p07a1330@yandex.ru>
2023-12-14 23:47:56 +0300

    with upload and crop handlers

--- src/pages/profile/column.jsx
+++ src/pages/profile/column.jsx
@@ -1,10 +1,21 @@
 import style from "./style.module.scss"
-
+import crop from "../../static/crop.svg"
+import upload from "../../static/upload.svg"
 export const Column = ({profilePhoto, content}) => {
+    const cropHandler = ()=>{
+
+    }
+    const uploadHandler = ()=>{
+
+    }
     return <div className={style.columnWrapper}>
         {profilePhoto && <>
             <div className={style.profilePhoto}>
                 <img src={profilePhoto}/>
+                <div className={style.controlsWrapper}>
+                    <div className={style.crop}><img src={crop} onClick={cropHandler}/> </div>
+                    <div className={style.upload}><img src={upload} onClick={uploadHandler}/> </div>
+                </div>
             </div>
         </>}
         <div className={style.body}>
