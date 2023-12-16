6199672c1a97faa521e3cbd3564895493bbc9401
p07a1330 <p07a1330@yandex.ru>
2023-12-16 15:51:38 +0300

    with search header

--- src/pages/faq/faq.jsx
+++ src/pages/faq/faq.jsx
@@ -1,5 +1,24 @@
-export const Faq = ()=>{
-    return <div>
-        i am faq page
+import style from "./style.module.scss"
+import {useState} from "react";
+
+export const Faq = () => {
+    const [searchValue, setSearchValue] = useState("")
+    return <div className={style.wrapper}>
+        <div className={style.header}>
+            <div className={style.title}>Чем вам помочь?</div>
+            <div className={style.search}>
+                <div className={style.searchInput}>
+                    <input value={searchValue} placeholder={"Поиск в справке"}
+                           onChange={(e) => setSearchValue(e.target.value)}/>
+                </div>
+                <div className={style.searchButton}>Найти</div>
+            </div>
+        </div>
+        <div className={style.questionList}>
+
+        </div>
+        <div className={style.answer}>
+
+        </div>
     </div>
 }
