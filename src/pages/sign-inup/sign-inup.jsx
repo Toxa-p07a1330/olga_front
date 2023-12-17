6abcd09321418a4c0af34ca25c7eea53e017b627
p07a1330 <p07a1330@yandex.ru>
2023-12-17 17:24:55 +0300

    with simple auth page

--- src/pages/sign-inup/sign-inup.jsx
+++ src/pages/sign-inup/sign-inup.jsx
@@ -1,5 +1,57 @@
-export  const SignInup = ()=>{
+import {useState} from "react";
+import style from "./style.module.scss"
+import classnames from "classnames"
+
+export const SignInup = () => {
+    const [isRegistration, setIsRegistration] = useState(false)
+    const toggleRegMode = () => {
+        setIsRegistration(!isRegistration)
+    }
+    if (true)
+        return <div>
+            <div className={style.form}>
+                <div className={style.title}>
+                    BigBroBrain
+                </div>
+                <div className={style.modeSelector}>
+
+                    <div className={classnames(style.singleMode, !isRegistration ? style.activeMode : "")}
+                         onClick={() => {
+                             !isRegistration && toggleRegMode()
+                         }}>
+                        Вход
+                    </div>
+                    <div className={classnames(style.singleMode, isRegistration ? style.activeMode : "")}
+                         onClick={() => {
+                             isRegistration && toggleRegMode()
+                         }}>
+                        Регистрация
+                    </div>
+                </div>
+                <div className={style.email}>
+                    <input placeholder={isRegistration ? "Имя" : "Логин или email"}/>
+                </div>
+
+                <div className={style.email}>
+                    <input placeholder={isRegistration ? "Email" : "Пароль"}/>
+                </div>
+                <div className={style.regButton}>
+                    {isRegistration ? "Зарегистрировать" : "Войти"}
+                </div>
+                <div className={style.rulesWrapper}>
+                    Правила
+                </div>
+                <div className={style.delimiter}>
+
+                </div>
+                <div className={style.googleAuth}>
+                    Войти с помощью гугл
+                </div>
+            </div>
+        </div>
     return <div>
-        I am sing in/up page
+        <div className={style.form}>
+            <div></div>
+        </div>
     </div>
 }
